bdd4e2c4b3a57ac70321267da08d2001a1172133
John Manoogian III <jm3@140proof.com>
2014-03-22 22:47:24 -0700

    split out js + add simple css

---
+++ source/javascripts/graphs.js
@@ -0,0 +1,21 @@
+var csv_uri = "https://docs.google.com/a/jm3.net/spreadsheet/pub?" 
+  + "key=0Aq5dBJnSM4yYdHVQQ1FmV1hSNzUwRG82MTFWWl9KNUE&output=csv";
+
+queue()
+  .defer( d3.csv, csv_uri)
+  .await(ready);
+
+function ready( error, data ) {
+
+d3.select(".dummy")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return (50 + d['Place'] * 10) + "px"; })
+    .text(function(d) { return d['Meet'] + " " + d['Place']; });
+
+  $.each(data, function(index, val) {
+    console.log(val)
+  });
+};
+
