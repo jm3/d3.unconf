ecce34bf1a17306e70fd232a3fba709d19722316
John Manoogian III <jm3@140proof.com>
2014-03-29 00:05:53 -0700

    render in SVG *and* in DIVs

--- source/javascripts/waveform.js
+++ source/javascripts/waveform.js
@@ -14,7 +14,47 @@ d3.json( wave_uri, function(error, json) {
 function svg_render( data, svg ) {
   var node = d3.select(svg).append("svg").attr("width", svg_width).attr("height", svg_height);
   d( "svg" );
-  // TBD: render SVG
+
+  var width = 960,
+  height = 50;
+
+  var y = d3.scale.linear().range([height, 0]);
+
+  var chart = node
+    .attr("width", width)
+    .attr("height", height);
+
+    y.domain([0, d3.max(data, function(d) { return d; })]);
+
+    var barWidth = width / data.length;
+
+    var bar = chart.selectAll("g")
+        .data(data)
+      .enter().append("g")
+        .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
+
+    bar.append("rect")
+      .attr("y", 0)
+      .attr("height", function(d) { 
+      h = (height - y(d)) + "px";
+      return y(d) + "px"; })
+      .attr("width", barWidth - 1);
+
+  /*
+    bar.append("text")
+      .attr("x", barWidth / 2)
+      .attr("y", function(d) { 
+        yval = (y(d) + 3) + "px";
+        return yval;
+      })
+      .attr("dy", ".75em")
+      .text(function(d) { return d; });
+  */
+
+  function type(d) {
+    d.value = +d.value; // coerce to number
+    return d;
+  }
 
 }
 
