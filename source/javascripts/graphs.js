e74034fc2a46d37245fc99b8e2c5c90b8e7ce574
John Manoogian III <jm3@140proof.com>
2014-03-28 22:40:38 -0700

    swim data

--- source/javascripts/graphs.js
+++ source/javascripts/graphs.js
@@ -1,5 +1,4 @@
-var csv_uri = "https://docs.google.com/a/jm3.net/spreadsheet/pub?" 
-  + "key=0Aq5dBJnSM4yYdHVQQ1FmV1hSNzUwRG82MTFWWl9KNUE&output=csv";
+var csv_uri = "https://docs.google.com/a/jm3.net/spreadsheet/pub?" + "key=0Aq5dBJnSM4yYdHVQQ1FmV1hSNzUwRG82MTFWWl9KNUE&output=csv";
 
 queue()
   .defer( d3.csv, csv_uri)
@@ -9,11 +8,12 @@ function ready( error, data ) {
 
   d3.select(".dummy")
     .selectAll("div")
-      .data(data, function(d) { return d['Date']+d['Time']; })
+      .data(data, function(d) { return d.Date+d.Time; })
     .enter().append("div")
-      .style("width", function(d) { return (50 + d['Place'] * 10) + "px"; })
-      .style("color", function(d) { return (50 + d['Place'] * 10) + "px"; })
-      .text(function(d) { return d['Meet'] + ": " + d['Place']; });
+      .style("width", function(d) { return (50 + d.Place * 10) + "px"; })
+      .style("color", function(d) { return (50 + d.Place * 10) + "px"; })
+      .text(function(d) { return d.Meet + ": " + d.Place; });
 
   // dump data: $.each(data, function(index, val) { console.log(val) });
-};
+}
+
