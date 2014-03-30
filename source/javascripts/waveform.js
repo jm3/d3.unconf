29a4e378ecb7e24b8b2d0f62072642e1cafcb65a
John Manoogian III <jm3@140proof.com>
2014-03-30 01:34:45 -0700

    add y-axis labels

--- source/javascripts/waveform.js
+++ source/javascripts/waveform.js
@@ -6,13 +6,13 @@ var wave_uri = "http://public.jm3.net/d3/geiger.json";
 var max_points = 512;
 
 var width = 960,
-height    = 60;
+height    = 140;
 
 // ints ranging from -32,767 to 32,767
 d3.json( wave_uri, function(error, json) {
   wave_json = json.data.slice(1, max_points);
-  div_render( wave_json, ".waveform > div" );
-  svg_render( wave_json, ".waveform" );
+  div_render( wave_json, ".waveform >  div" );
+  svg_render( wave_json, ".waveform > .svg" );
 });
 
 function div_render( data, div ) {
@@ -27,7 +27,7 @@ function div_render( data, div ) {
     .enter()
     .append("div")
     .attr("class", "bar")
-    .style("width", 2 + "px" )
+    .style("width", bar_width + "px" )
     .style("height", function(d,i) {
       return y(d) + "px";
     })
@@ -40,14 +40,32 @@ function div_render( data, div ) {
       var left =  x(i)*width; // could also do: i * bar_width;
       return left + "px";
     });
+
+
+  var axis = d3.svg.axis()
+    .scale(y)
+    .ticks(12)
+    .orient("left");
+
+  d3.select(".waveform > div").append("svg")
+    .attr("class", "axis")
+    .attr("width", 60)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(40," + (height/2) + ")" )
+    .call(axis);
 }
 
 function svg_render( data, svg ) {
-  var node = d3.select(svg).append("svg").attr("width", width).attr("height", height);
+  var node = d3.select(svg).append("svg")
+    .attr("class","chart")
+    .attr("width", width)
+    .attr("height", height);
 
   var y = d3.scale.linear().range([height, -height]);
   var max_val = d3.max(data, function(d) { return d; });
   y.domain([-max_val, max_val]);
+  var x = d3.scale.linear().domain([0, data.length]);
   var bar_width = width / data.length;
 
   var chart = node.attr("width", width).attr("height", height);
@@ -65,8 +83,21 @@ function svg_render( data, svg ) {
       return yv;
     })
     .attr("height", function(d) {
-      return y(d); })
-    .attr("width", Math.abs(bar_width ));
+      return Math.abs(y(d)); })
+    .attr("width", bar_width );
+
+  var axis = d3.svg.axis()
+    .scale(y)
+    .ticks(12)
+    .orient("left");
+
+  d3.select(".svg").append("svg")
+    .attr("class", "axis")
+    .attr("width", 60)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(40," + (height/2) + ")" )
+    .call(axis);
 }
 
 function d( s ) {
