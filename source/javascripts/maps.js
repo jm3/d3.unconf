fb3e304e8d344f61c8919deb3867c1b0fa7da645
John Manoogian III <jm3@140proof.com>
2014-03-31 14:26:51 -0700

    preliminary map rendering

--- source/javascripts/maps.js
+++ source/javascripts/maps.js
@@ -1,27 +1,27 @@
-var width  = 960,
-    height = 960;
-
-function chart_it(options) {
-
-  var width       = options.width;
-  var height      = options.height;
-  var latLong     = options.latLong;
-  var scale       = options.scale;
-  var geoDataUrl  = options.geoDataUrl;
-  var statDataUrl = options.statDataUrl;
+function map_it(options) {
+  var width          = options.width;
+  var height         = options.height;
+  var latLong        = options.latLong;
+  var injectInto     = options.injectInto;
+  var collectionName = options.collectionName;
+  var scale          = options.scale;
+  var geoDataUrl     = options.geoDataUrl;
+  var statDataUrl    = options.statDataUrl;
 
   var lng = latLong[0];
   var lat = latLong[1];
 
-  var svg = d3.select("body").append("svg")
+  var svg = d3.select(injectInto).append("svg")
       .attr("width", width)
       .attr("height", height);
 
   d3.json(geoDataUrl, function(error, geodata) {
     d = geodata;
-    console.log("loaded", d.objects.hoods.geometries.length, "features");
+      console.log(d.objects);
+    console.log(collectionName, ": loaded", 
+      d.objects[collectionName].geometries.length, "features");
 
-    var projection = d3.geo.albers()
+    var projection = d3.geo.mercator()
       .translate([(width / 2), (height / 2)])
       .scale(scale)
       .rotate([-lng,0])
@@ -29,23 +29,45 @@ function chart_it(options) {
 
     var path = d3.geo.path().projection(projection);
 
-    // draw the hoods
     svg.selectAll("path")
-      .data(topojson.feature(geodata, geodata.objects.hoods).features)
+      .data(topojson.feature(geodata, geodata.objects[collectionName]).features)
       .enter().append("path")
-      .attr("fill", "red")
-      .attr("stroke", "hsla(200,50%,50%,0.6)")
+      .attr("fill", "#ddd")
+      .attr("stroke", "hsla(200,50%,50%,0.3)")
       .attr("d", path );
 
   });
 }
 
-chart_it({
-  width: 960,
-  height: 960,
-  latLong: [-118.2428,34.00],
-  scale: 7000,
-  geoDataUrl: "./maps/sf-hoods.topojson",
-  statDataUrl: "./TBD.csv"
+map_it({
+  width:           500,
+  height:          500,
+  injectInto:      ".sf-hoods",
+  collectionName:  "sf-hoods",
+  latLong:         [-114.00, 40.00], // good for sf-hoods-topo
+  scale:           1200, // good for sf-hoods-topo
+  statDataUrl:     "./TBD.csv",
+  geoDataUrl:      "./maps/sf-hoods-topo.json"
 });
 
+map_it({
+  width:           700,
+  height:          500,
+  injectInto:      ".states",
+  collectionName:  "states",
+  latLong:         [-100.00, 34.00], // good for states-topo
+  scale:           500, // good for states-topo
+  statDataUrl:     "./TBD.csv",
+  geoDataUrl:      "./maps/states-topo.json"
+});
+
+map_it({
+  width:           500,
+  height:          500,
+  injectInto:      "body",
+  collectionName:  "sfpd-SOMA-resolved",
+  latLong:         [-114.00, 40.00], // good for sf-hoods-topo
+  scale:           1200, // good for sf-hoods-topo
+  statDataUrl:     "./TBD.csv",
+  geoDataUrl:      "./maps/sfpd-SOMA-resolved-topo.json"
+});
