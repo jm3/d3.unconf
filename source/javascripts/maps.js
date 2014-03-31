d6d924d6328563d5f09f83edeb07982d4a6e9008
John Manoogian III <jm3@140proof.com>
2014-03-31 15:35:00 -0700

    kinda, sorta, ham-fistedly align maps

--- source/javascripts/maps.js
+++ source/javascripts/maps.js
@@ -5,15 +5,18 @@ function map_it(options) {
   var injectInto     = options.injectInto;
   var collectionName = options.collectionName;
   var scale          = options.scale;
+  var fill           = options.fill;
+  var stroke         = options.stroke;
   var geoDataUrl     = options.geoDataUrl;
   var statDataUrl    = options.statDataUrl;
 
   var lng = latLong[0];
   var lat = latLong[1];
 
-  var svg = d3.select(injectInto).append("svg")
-      .attr("width", width)
-      .attr("height", height);
+  var svg;
+    svg = d3.select(injectInto).append("svg")
+    .attr("id",collectionName)
+    .attr("width", width).attr("height", height);
 
   d3.json(geoDataUrl, function(error, geodata) {
     d = geodata;
@@ -21,7 +24,7 @@ function map_it(options) {
     console.log(collectionName, ": loaded", 
       d.objects[collectionName].geometries.length, "features");
 
-    var projection = d3.geo.mercator()
+    var projection = d3.geo.albers()
       .translate([(width / 2), (height / 2)])
       .scale(scale)
       .rotate([-lng,0])
@@ -32,8 +35,8 @@ function map_it(options) {
     svg.selectAll("path")
       .data(topojson.feature(geodata, geodata.objects[collectionName]).features)
       .enter().append("path")
-      .attr("fill", "#ddd")
-      .attr("stroke", "hsla(200,50%,50%,0.3)")
+      .attr("fill", fill)
+      .attr("stroke", stroke)
       .attr("d", path );
 
   });
@@ -42,10 +45,12 @@ function map_it(options) {
 map_it({
   width:           500,
   height:          500,
-  injectInto:      ".sf-hoods",
+  fill:            "#ddd",
+  stroke:          "hsla(200,50%,50%,0.3)",
+  injectInto:      "svg.sf",
   collectionName:  "sf-hoods",
-  latLong:         [-114.00, 40.00], // good for sf-hoods-topo
-  scale:           1200, // good for sf-hoods-topo
+  latLong:         [-114.00, 40.00],
+  scale:           1200,
   statDataUrl:     "./TBD.csv",
   geoDataUrl:      "./maps/sf-hoods-topo.json"
 });
@@ -53,10 +58,12 @@ map_it({
 map_it({
   width:           700,
   height:          500,
-  injectInto:      ".states",
+  fill:            "#ddd",
+  stroke:          "hsla(200,50%,50%,0.3)",
+  injectInto:      "svg.usa",
   collectionName:  "states",
-  latLong:         [-100.00, 34.00], // good for states-topo
-  scale:           500, // good for states-topo
+  latLong:         [-100.00, 34.00],
+  scale:           500,
   statDataUrl:     "./TBD.csv",
   geoDataUrl:      "./maps/states-topo.json"
 });
@@ -64,10 +71,12 @@ map_it({
 map_it({
   width:           500,
   height:          500,
-  injectInto:      "body",
+  fill:            "#FF0000",
+  stroke:          "#333",
+  injectInto:      "svg.sf",
   collectionName:  "sfpd-SOMA-resolved",
-  latLong:         [-114.00, 40.00], // good for sf-hoods-topo
-  scale:           1200, // good for sf-hoods-topo
+  latLong:         [-122.40, 37.70],
+  scale:           1200,
   statDataUrl:     "./TBD.csv",
   geoDataUrl:      "./maps/sfpd-SOMA-resolved-topo.json"
 });
