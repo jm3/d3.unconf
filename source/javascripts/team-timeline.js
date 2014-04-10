30bcab8452a275a15b0708dbdc79f0a35178a0e3
John Manoogian III <jm3@140proof.com>
2014-04-10 00:45:49 -0700

    add time scale, hover, and click

--- source/javascripts/team-timeline.js
+++ source/javascripts/team-timeline.js
@@ -14,22 +14,42 @@ function init(options) {
         var end   = new Date( d["Release Date"] ).getTime();
         if( end > start ) {
           team_data.push({
-            icon: icon_url_prefix + (d["Icon"] || "missing") + ".png",
-            times: [{"starting_time": new Date( d["Hire Date"] ).getTime(), "ending_time": new Date( d["Release Date"] ).getTime()}]
+            icon:   icon_url_prefix + (d["Icon"] || "missing") + ".png",
+            label:  d["Employee"],
+            id:     d["Employee"].replace(/ /g, "_"),
+            times:  [{ "starting_time": start, "ending_time": end }]
           });
         }
       });
 
       function tl() {
+
+        var formatTime = d3.time.format("%Y"),
+            formatMonths = function(d) { return formatTime(new Date(1970, 0, 1, 0, 0, 0, d)); };   //The intervals are in millis
+
         var chart = d3.timeline()
+          .tickFormat({
+            format: formatMonths,
+            tickTime: d3.time.month,
+            tickInterval: 12,
+            tickSize: 1,
+          })
           .beginning( new Date( "Jul 21, 2009" ).getTime()) // we can optionally add beginning and ending times to speed up rendering a little
           .ending(    new Date( "Apr  8, 2014" ).getTime())
-          .stack() 
+          .stack()
           .margin({left:70, right:30, top:0, bottom:0})
+          .hover(function (d, i, datum) {
+            var name = $('.who .name');
+            name.text( datum.label );
+            var icon = $('.who img.icon');
+            icon.attr( "src", datum.icon );
+          })
+          .click(function (d, i, datum) {
+            alert(datum.label);
+          })
           ;
-        var svg = d3.select(".timeline").append("svg").attr("width", width)
-          .datum(team_data).call(chart);
-  return;
+        var svg = d3.select(".timeline").append("svg").attr("width", width).datum(team_data).call(chart);
+        $("#timelineItem_product").css("fill","black");
       }
       tl();
 
